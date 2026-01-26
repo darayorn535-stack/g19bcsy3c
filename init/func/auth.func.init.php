c72b7a2ef5c1d5032e95510f44edcff777b049d4
phathygh <phathy.nap@gmail.com>
2026-01-26 08:40:01 +0700

    update code

---
+++ init/func/auth.func.init.php
@@ -0,0 +1,28 @@
+<?php
+   
+
+function usernameExists($username) {
+    global $db;
+    $query = $db->prepare('SELECT * FROM tbl_users WHERE username = ?');
+    $query ->bind_param("s",$username);
+    $query-> execute();
+    $result = $query->get_result();
+    if($result->num_rows){
+        return true;
+    }
+    return false; // គេហៅថា return erlly​ចេញប្រឡង
+}
+
+function registerUser($name,$username,$passwd){
+    global $db;
+    if(usernameExists($username)){
+        return false;
+    }
+    $query = $db->prepare('INSERT INTO tbl_users  (name, username, passwd)VALUES(?,?,?)');
+    $query->bind_param('sss',$name,$username,$passwd);
+    $query->execute();
+    if($query->affected_rows){
+        return true;
+    }
+    return false;
+}
--- pages/register.php
+++ pages/register.php
@@ -2,10 +2,10 @@
 $name = $username = $passwd = '';
 $nameErr = $usernameErr = $passwdErr = '';
 if(isset($_POST['name'],$_POST['username'],$_POST['passwd'],$_POST['confirmPasswd'])){
-    $name = $_POST['name'];
-    $username = $_POST['username'];
-    $passwd = $_POST['passwd'];
-    $confirmPasswd = $_POST['confirmPasswd'];
+    $name = trim($_POST['name']);
+    $username = trim($_POST['username']);
+    $passwd = trim($_POST['passwd']);
+    $confirmPasswd = trim($_POST['confirmPasswd']);
     if(empty($name)){
         $nameErr = 'pleas input name!';
     }
@@ -17,6 +17,24 @@ if(isset($_POST['name'],$_POST['username'],$_POST['passwd'],$_POST['confirmPassw
     }
     if(empty($confirmPasswd)){
         $passwdErr = 'passwd not match!';
+    }
+   if(usernameExists($username)){
+     $usernameErr = 'Username exist!';
+   }
+   if(empty($nameErr) && empty($usernameErr) && empty($passwdErr)){
+        if(registerUser($name, $username, $passwd)){
+            $name = $username = $passwd ='';
+            echo '<div class="alert alert-success" role="alert">
+                    Registered. Go to <a href="./?page=login">Login</a>
+                    </div>';
+            // header('Location: ./?page=login');
+        }else{
+            echo '<div class="alert alert-danger" role="alert">
+                Username exists or Service busy!
+                </div>';
+        }
+
+        
     }
 
 }
